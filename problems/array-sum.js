d81fabd71c562eb1c7f16a0173971b953853e704
sunnystevenson10 <sunnystevenson10@gmail.com>
2015-09-29 08:31:15 -0600

    Quiz 2

--- problems/array-sum.js
+++ problems/array-sum.js
@@ -1,6 +1,18 @@
 'use strict';
 
 function arraySum(arr) {
+var total = 0;
+
+for (var i = 0; i < arr.length; i++) {
+  var element = arr[i];
+  if (typeof element === 'object') {
+    total = total + arraySum(element);
+  } else {
+    total = total + element;
+    }
+  }
+
+return total;
 
 }
 
--- problems/bind-name.js
+++ problems/bind-name.js
@@ -1,7 +1,8 @@
 'use strict';
 
 function bindName(firstName, lastName, func) {
-
+  var name = firstName + ' ' + lastName;
+  return func.bind(null, name);
 }
 
 module.exports = bindName;
